c2b478e0bf25bdcd770eb1bbca18106245644097
Ahmed Shoura <ahmedshoura1996@gmail.com GitHub>
2026-07-21 13:38:02 +0300

    Add initial project configuration

---
+++ scripts/project_info.py
@@ -0,0 +1,20 @@
+"""
+Project Information
+Master's Thesis
+
+Title:
+Reinforcement Learning for Peg-in-Hole Assembly
+using NVIDIA Isaac Sim
+"""
+
+PROJECT_NAME = "RL-PegInHole-IsaacSim"
+
+AUTHOR = "Ahmed Shoura"
+
+ROBOT = "Franka Panda"
+
+SIMULATOR = "NVIDIA Isaac Sim"
+
+RL_ALGORITHM = "Soft Actor-Critic (SAC)"
+
+VERSION = "0.1.0"
